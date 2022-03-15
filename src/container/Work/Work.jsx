a1541947c77eb29ef655244f2545f5c41a9584f9
Patrik Přidal <98656826+vibemane@users.noreply.github.com>
2022-03-15 13:27:00 +0100

    motion div for pic, description, and <a></a> for link on project and link on github project of that project

--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -7,8 +7,21 @@ import { urlFor, client } from "../../client";
 import "./Work.scss";
 
 const Work = () => {
-  const [activeFilter, setActiveFilter] = useState('All')
+  const [activeFilter, setActiveFilter] = useState('All');
+  const [animateCard, setAnimateCard] = useState({ y: 0, opacity: 1 });
+  const [works, setWorks] = useState([]);
+  const [filterWork, setFilterWork] = useState([]);
 
+  useEffect(() => {
+    const query = '*[_type == "works"]'
+
+    client.fetch(query)
+      .then((data) => {
+        setWorks(data);
+        setFilterWork(data);
+      })
+  }, [])
+  
   const handleWorkFilter = (item) => {
 
   }
@@ -28,6 +41,31 @@ const Work = () => {
           </div>
         ))}
       </div>
+
+      <motion.div
+        animate={animateCard}
+        transition={{ duration: 0.5, delayChildren: 0.5 }}
+        className="app__work-portfolio"
+      >
+        {filterWork.map((work, index) =>(
+          <div className="app__work-item app__flex" key={index}>
+            <div className="app__work-img app__flex">
+              <img src={urlFor(work.imgUrl)} alt={work.name} />
+
+              <motion.div
+                whileHover={{ opacity: [0, 1] }}
+                transition={{ duration: 0.25, ease: 'easeInOut', staggerChildren: 0.5 }}
+                className="app__work-hover app__flex"
+              >
+                <a href={work.projectLink} target="_blank" rel="noreferrer">
+                  
+                </a>
+
+              </motion.div>
+            </div>
+          </div>
+        ))}
+      </motion.div>
     </>
   );
 };
