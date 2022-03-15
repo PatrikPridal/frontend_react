6bbeb7b3808443eff0aefecf4f83e3b93a7dca77
Patrik Přidal <98656826+vibemane@users.noreply.github.com>
2022-03-15 16:29:03 +0100

    useEffect for fetching data from Sanity studio

--- src/container/Testimonial/Testimonial.jsx
+++ src/container/Testimonial/Testimonial.jsx
@@ -12,6 +12,21 @@ import './Testimonial.scss';
 const Testimonial = () => {
   const [brands, setBrands] = useState([]);
   const [testimonials, setTestimonials] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const query = '*[_type == "testimonials"]';
+    const brandsQuery = '*[_type == "brands"]';
+
+    client.fetch(query).then((data) => {
+      setTestimonials(data);
+    });
+
+    client.fetch(brandsQuery).then((data) => {
+      setBrands(data);
+    });
+  }, []);
+
 
   return (
     <div>Testimonial</div>
