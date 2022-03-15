1ab335c17728a098dea47c585d13c7e7096b532a
Patrik Přidal <98656826+vibemane@users.noreply.github.com>
2022-03-15 16:26:11 +0100

    imports in Testimonials.jsx

--- src/container/Testimonial/Testimonial.jsx
+++ src/container/Testimonial/Testimonial.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+//external imports
+import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
+import { HiChevronLeft, HiChevronRight } from 'react-icons/hi';
 
+//internal imports
+import { AppWrap, MotionWrap } from '../../wrapper';
+import { urlFor, client } from '../../client';
 import './Testimonial.scss';
 
 const Testimonial = () => {
+  const [brands, setBrands] = useState([]);
+  const [testimonials, setTestimonials] = useState([]);
+
   return (
     <div>Testimonial</div>
   )
