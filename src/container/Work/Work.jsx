1802fd26c697951048faca62dbce99c119ba4f31
Patrik Přidal <98656826+vibemane@users.noreply.github.com>
2022-03-15 13:42:12 +0100

    data prepared

--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -67,19 +67,29 @@ const Work = () => {
                     <AiFillEye />
                   </motion.div>
                 </a>
-                <a href={work.projectLink} target="_blank" rel="noreferrer">
+                <a href={work.codeLink} target="_blank" rel="noreferrer">
                   <motion.div
                     whileInView={{ scale: [0, 1] }}
                     whileHover={{ scale: [1, 0.9] }}
                     transition={{ duration: 0.25 }}
                     className="app__flex"  
                   >
-                    <AiFillEye />
+                    <AiFillGithub />
                   </motion.div>
                 </a>
 
               </motion.div>
             </div>
+
+            <div className="app__work-content app__flex">
+              <h4 className="bold-text">{work.title}</h4>
+              <p className="p-text" style={{ marginTop: 10 }}>{work.description}</p>
+
+              <div className="app__work-tag app__flex">
+                <p className="p-text">{work.tags[0]}</p>
+              </div>
+            </div>
+
           </div>
         ))}
       </motion.div>
@@ -87,4 +97,4 @@ const Work = () => {
   );
 };
 
-export default Work;
+export default AppWrap(Work, 'work');
