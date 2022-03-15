b8dc9bb10758ab9bca81655097f50e736c611988
Patrik Přidal <98656826+vibemane@users.noreply.github.com>
2022-03-15 14:58:31 +0100

    added rendering of motion divs and div + p tag

--- src/container/Skills/Skills.jsx
+++ src/container/Skills/Skills.jsx
@@ -4,48 +4,47 @@ import ReactTooltip from "react-tooltip";
 
 import { AppWrap } from "../../wrapper";
 import { urlFor, client } from "../../client";
-import './Skills.scss';
+import "./Skills.scss";
 
 const Skills = () => {
   const [experience, setExperience] = useState([]);
   const [skills, setSkills] = useState([]);
 
   useEffect(() => {
-    const query = '*[_type == "experiences"]'
-    const skillsQuery = '*[_type == "skills"]'
+    const query = '*[_type == "experiences"]';
+    const skillsQuery = '*[_type == "skills"]';
 
-    client.fetch(query)
-      .then((data) => {
-        setExperience(data);
-        
-      })
-    client.fetch(skillsQuery)
-      .then((data) => {
-        setSkills(data);
-        
-      })
-  }, [])
+    client.fetch(query).then((data) => {
+      setExperience(data);
+    });
+    client.fetch(skillsQuery).then((data) => {
+      setSkills(data);
+    });
+  }, []);
 
   return (
     <>
-      <h2 className="head-text">Skills & Experience</h2>
+      <h2 className='head-text'>Skills & Experience</h2>
 
-      <div className="app__skills-container">
-        <motion.div className="app__skills-list">
+      <div className='app__skills-container'>
+        <motion.div className='app__skills-list'>
           {skills.map((skill) => (
-            <motion.div 
-              whileHover={{opacity: [0, 1]}}
+            <motion.div
+              whileHover={{ opacity: [0, 1] }}
               transition={{ duration: 0.5 }}
-              className="app__skills-item app__flex"
+              className='app__skills-item app__flex'
               key={skill.name}
             >
-
+              <div className='app__flex' style={{ backgroundColor: skill.bgColor }}>
+                <img src={urlFor(skill.icon)} alt={skill.name} />
+              </div>
+              <p className='p-text'>{skill.name}</p>
             </motion.div>
           ))}
         </motion.div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Skills
+export default Skills;
